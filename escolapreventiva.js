214c830ba3ad22f31cb9054687d4ea42f9c193df
Francisco Darlan <118319737+FranciscoDarlan@users.noreply.github.com>
2024-07-16 01:00:17 -0300

    Create escolapreventiva.js

---
+++ escolapreventiva.js
@@ -0,0 +1,176 @@
+Então, eu preciso usar nosso tempo hoje para explicar o que é NR geral / especial / setorial, pois não sei se você tem esse conhecimento
+
+De um jeito simples:
+NRs Gerais:
+Se aplicam a todas as empresas, ou seja, não estão condicionadas a um setor ou uma atividade (NRs 01, 03, 04, 05, 07, 09, 17, 28)
+
+ 
+
+NRs Especiais:
+Se aplicam em atividades específicas, como eletricidade, movimentação de cargas, operação de máquinas, etc.
+
+(NRs 06, 08, 10, 11, 12, 13, 14, 15, 16, 19, 20, 21, 23, 24, 25, 26, 33, 35)
+
+ 
+
+NRs Setoriais:
+Se aplicam a setores da economia, como agricultura, obras, limpeza urbana, hospitais, etc.
+(NRs 18, 22, 29, 30, 31, 32, 34, 36, 37, 38)
+
+___________________________________________________________________________________________________________________
+
+Bom dia Profissional SST.
+
+Como te expliquei ontem, hoje vou te passar a PARTE 1 do seu Programa GERAL de Treinamentos SST.
+Quando uma empresa te pedir um plano de treinamentos para atendimento das NRs você pode quebrar em 3 partes e hoje vou te passar a parte 1.
+Na parte 1 você vai entregar treinamentos das NRs gerais, mas não todas. 
+
+Porque a nossa premissa, como expliquei nos emails anteriores, é que a empresa está dando os primeiros passos, então tem que focar nas NRs mais prioritárias.
+Então na parte 1 você vai falar das NRs 01, 05 e 17.
+Vai ficar de fora 03, 04, 07, 09 e 28 pois são temas mais técnicos e que não envolve tanto os trabalhadores.
+
+Então seu Programa GERAL de Treinamentos SST (Parte 1) poderia ficar assim:
+NR-01: Disposições Gerais e Gerenciamento de Riscos Ocupacionais
+(OBS: embora a NR-01 cite os treinamentos inicial, periódicos e eventuais, eles serão ministrados cada um na sua NR específica)
+Treinamentos:
+Treinamento de integração para novos empregados.
+
+NR-05: Comissão Interna de Prevenção de Acidentes (CIPA)
+(OBS: mesmo empresas que não precisem formar CIPA precisarão indicar um designado. Então de um jeito ou de outro tem pelo menos um trabalhador para treinar. )
+Treinamentos:
+Treinamento para membros da CIPA, abordando temas como noções de prevenção de acidentes e doenças ocupacionais, metodologia de investigação e análise de acidentes, e noções sobre legislação trabalhista, entre outros temas.
+
+NR-17: Ergonomia
+Treinamentos:
+Orientações sobre práticas ergonômicas no ambiente de trabalho.
+Treinamentos específicos para a prevenção de distúrbios musculoesqueléticos.
+Instruções sobre a correta postura e movimentação durante as atividades laborais.
+Treinamento específico para operadores de checkout
+
+Muito bem! 
+Com isso fechamos a parte 1 onde focamos nas NRs gerais mais relevantes para o nosso caso.
+No e-mail de amanhã, quinta, vou te passar a parte 2 onde trataremos das NRs especiais.
+Até amanhã.
+
+______________________________________________________________________________________________________________________________
+
+Olá, bom dia! 
+
+Hoje vou te passar a PARTE 2 do seu Programa GERAL de Treinamentos SST.
+( a parte 1 eu te mandei ontem ... recomendo ler meus emails que enviei essa semana, caso já não tenha lido )
+Quando uma empresa te pedir um plano de treinamentos para atendimento das NRs você pode quebrar em 3 partes e hoje vou te passar a parte 2.
+Um detalhe: nem todos os treinamentos se aplicam a todas as empresas.
+Exemplo: NR-12 Máquinas, NR-11 movimentação de cargas, etc.
+Então retire da lista a seguir tudo que não se aplica ao seu cliente.
+Além disso, vou deixar de fora as NRs 08, 14, 15, 16, 19, 21, 24, 25 e 26. 
+Motivo: julguei que um plano inicial de treinamentos de trabalhadores essas NRs são mais raras ou menos prioritárias.
+Então sugiro incluir essas mais relevantes: NRs 06, 10, 11, 12, 13, 20, 23, 33 e 35.
+
+NR-06: Equipamento de Proteção Individual (EPI)
+Treinamentos:
+Orientação sobre uso, conservação e higienização dos EPIs.
+Treinamentos práticos para o correto uso dos EPIs específicos de cada atividade.
+ NR-10: Segurança em Instalações e Serviços em Eletricidade
+
+Treinamentos
+Curso básico (40 horas) para trabalhadores que interagem com instalações elétricas.
+Curso Complementar SEP (Básico + 40 Horas) 
+NR-11: Transporte, Movimentação, Armazenagem e Manuseio de Materiais
+Treinamentos:
+Treinamentos específicos para operadores de empilhadeiras e outros equipamentos de movimentação de carga, focando na operação segura, manutenção preventiva e manuseio correto.
+Orientações sobre técnicas seguras de armazenagem e empilhamento de materiais para prevenir acidentes.
+
+NR-12: Segurança no Trabalho em Máquinas e Equipamentos
+Treinamentos:
+Treinamentos específicos para a operação segura de cada tipo de máquina.
+Instruções sobre procedimentos de manutenção e inspeção.
+Atualização contínua conforme novas máquinas ou tecnologias são introduzidas.
+NR-13 Caldeiras e Vasos de Pressão
+Treinamento para operadores de caldeiras
+Treinamento para operadores de vasos de pressã
+NR-20 Segurança e Saúde no Trabalho com Inflamáveis e Combustíveis
+Treinamentos:
+Iniciação sobre Inflamáveis e Combustíveis
+Básico
+Intermediário
+Avançado I
+Avançado II
+Específico
+NR-23 Proteção Contra Incêndios
+Treinamentos:
+Instrutor de Brigada
+Primeiros socorros
+Exercícios simulados
+NR-33 Segurança e Saúde no Trabalho em Espaços Confinados
+Treinamentos:
+trabalhadores autorizados e vigias
+supervisores de entrada
+treinamento do responsável técnico pelos espaços confinados
+Resgate e emergência
+NR-35 Trabalho em Altura
+Treinamento para trabalhador que realiza trabalho em altura
+Com isso finalizamos a PARTE 2 do seu Programa GERAL de Treinamentos SST.
+Amanhã te mando a PARTE FINAL, a terceira onde trataremos das NRs Setoriais.
+Até amanhã.
+_______________________________________________________________________________________________________________
+
+
+Nem acredito
+Essa semana foi puxada.
+Minha ideia inicial era escrever 3 emais, mas acabei tendo que quebrar em 5 para ficar mais organizado!
+Como tem treinamento para ministrar na área SST.
+De fato esse é um grande mercado que muitos Profissionais SST exploram bem.
+Bem...
+Sem mais delongas vamos ao conteúdo de encerramento dessa semana que foi muito produtiva.
+Hoje vou te passar a PARTE 3 do seu Programa GERAL de Treinamentos SST.
+Vamos tratar das NRs 18, 22, 31, 32, 36, 37 e 38.
+
+Vou me abster de citar: NRs 29, 30, 34 e 37 pelo mesmo motivo do email de ontem.
+Motivo: além de ser setores mais raros de aparecer serviço, são NRs de maior complexidade e que exigem um plano mais detalhado. 
+Muito difícil uma empresa de 29, 30, 34 e 37 estar dando os "primeiros passos" na SST.
+É CLARO QUE você não vai falar de NR-18 lá no hospital, nem vai falar de NR-31 numa plataforma de petróleo.
+Eu vou nominar o treinamento daí você retira aquilo que não se aplica a empresa, combinado?
+NR-18 Indústria da Construção
+Treinamentos iniciais de integração
+Treinamentos específicos por função (grua, PEMT, cadeira suspensa, etc)
+
+NR-22 Mineração
+treinamento introdutório geral;
+treinamento específico na função.
+
+NR-31 Trabalho Rurall
+Todos os trabalhadores devem ser treinamentos, de acordo com suas atividades e funções. Em destaque podemos citar:
+uso de agrotóxicos
+treinamentos inicial, periódico e eventual, por função
+treinamento do cipeiro (CIPATR)
+operadores de motosserra e motopoda
+operadores de roçadeira costal motorizada e derriçadeira
+operadores de trator e similar
+trabalhador que realiza levantamento, manuseio e transporte manual regular de cargas
+
+NR-32 Serviços de saúde
+riscos biológicos (perigos, riscos e medidas de controle)
+riscos químicos
+radiações ionizantes
+uso seguro de perfurocortantes
+
+NR-36 Abate e Processamento de Carnes e Derivados
+Todos os trabalhadores devem ser treinados, incluindo:
+treinamento dos superiores hierárquicos
+trabalhadores que efetuam limpeza e desinfecção de materiais, equipamentos e locais de trabalho
+
+NR-38 Limpeza urbana
+Todos os trabalhadores envolvidos nas seguintes atividades:
+coleta, transporte e transbordo de resíduos sólidos urbanos
+coleta, transporte e transbordo de resíduos de serviços de saúde
+varrição e lavagem de feiras, vias e logradouros públicos;
+capina, roçagem e poda de árvores;
+manutenção de áreas verdes;
+raspagem e pintura de meio-fio;
+limpeza e conservação de mobiliário urbano, monumentos, 
+limpeza e conservação de túneis, pontes e viadutos;
+desobstrução e limpeza de bueiros, bocas de lobo e correlatos;
+triagem e manejo de resíduos sólidos urbanos recicláveis;
+limpeza de praias;
+pontos de recebimento de resíduos sólidos urbanos; e
+disposição final.
